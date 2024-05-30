dd168acc6f4a178ef187ce1f8c711ce9df60214a
themerous <lhs981130@gmail.com>
2024-05-30 14:13:01 +0900

    build: Created new Spring Boot project

---
+++ project2_bit701/src/main/java/com/project/bit701/Project2Bit701Application.java
@@ -0,0 +1,13 @@
+package com.project.bit701;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2Bit701Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2Bit701Application.class, args);
+	}
+
+}
